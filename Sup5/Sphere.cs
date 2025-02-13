269d62de313ed9baa3a2e5ead49c63e5f93fdffe
albertlluberes <albertlluberes123456789@gmail.com>
2025-02-13 14:50:02 -0500

    Implemented Sphere class with validation, volume, surface area, and dump

---
+++ Sup5/Sphere.cs
@@ -0,0 +1,40 @@
+using System;
+using Sup5;
+namespace Supplement5;
+
+public class Sphere : Shape3D
+{
+    private double radius;
+
+  
+    public Sphere(double radius)
+    {
+        Validate(radius);
+        this.radius = radius;
+    }
+
+    private void Validate(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentException("Radius must be greater than 0.");
+        }
+    }
+
+   
+    public override double GetVolume()
+    {
+        return (4 / 3.0) * Math.PI * Math.Pow(radius, 3);
+    }
+
+    
+    public override double GetSurfaceArea()
+    {
+        return 4 * Math.PI * Math.Pow(radius, 2);
+    }
+
+    public override string Dump()
+    {
+        return $"Shape: Sphere, Surface Area: {GetSurfaceArea()}, Volume: {GetVolume()}";
+    }
+}
