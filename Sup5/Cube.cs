ba0a32c62f131e698dcbc5643646c79e2408707b
albertlluberes <albertlluberes123456789@gmail.com>
2025-02-13 13:59:45 -0500

    Implement Cube class with validation and calculations

---
+++ Sup5/Cube.cs
@@ -0,0 +1,36 @@
+using System;
+using Sup5;
+
+public class Cube : Shape3D
+{
+    private double sideLength;
+
+    public Cube(double sideLength)
+    {
+        Validate(sideLength);
+        this.sideLength = sideLength;
+    }
+
+    private void Validate(double sideLength)
+    {
+        if (sideLength <= 0)
+        {
+            throw new ArgumentException("Side length must be greater than 0.");
+        }
+    }
+
+    public override double GetVolume()
+    {
+        return Math.Pow(sideLength, 3);
+    }
+
+    public override double GetSurfaceArea()
+    {
+        return 6 * Math.Pow(sideLength, 2);
+    }
+
+    public override string Dump()
+    {
+        return $"Shape: Cube, Surface Area: {GetSurfaceArea()}, Volume: {GetVolume()}";
+    }
+}
